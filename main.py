67e72809cbbcd6385785938e3c576a008999d9c4
Nekochort <swordfeed02@gmail.com>
2022-03-24 11:56:47 +0500

    REVERT ALL CHANGES

--- main.py
+++ main.py
@@ -1,40 +1,17 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-import psycopg2
-import os
 
-from config import TOKEN, DB_URI
+from config import TOKEN
 
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-conn = psycopg2.connect(DB_URI, ssl_mode = "require")
-cur = conn.cursor()
-
-# webhook settings
-HEROKU_APP_NAME = "itrevolutionbot"
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{TOKEN}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-
-# webserver settings
-WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
-
-
-async def on_startup(dispatcher):
-    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
-
-
-async def on_shutdown(dispatcher):
-    await bot.delete_webhook()
-
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши-ка мне любой текст и я его повторю!")
+    await message.reply("Привет!\nНапиши мне что-нибудь!")
 
 
 @dp.message_handler(commands=['help'])
